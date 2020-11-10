bd5bb5098123b11e98adb389858b560a27a209d3
ntarver0409 <nctarver4994@icloud.com>
2020-11-10 17:51:19 -0600

    Progress on bankAnalysis.py

--- PyBank/bankAnalysis.py
+++ PyBank/bankAnalysis.py
@@ -15,26 +15,34 @@ with open(csvpath, 'r') as bankRecords:
     dates = []
     profitLosses = []
     minMax = []
+    totalProfit = sum(profitLosses)
 
 #For loop to cycle through each column of data and assign the column to the variable array
     for row in bankRecords:
         dates.append(row[0])
-        profitLosses.append(row[1])
-
-    for i in range(1, len(minMax)):
-
-
-
-
-
-
+        profitLosses.append(float(row[1])
 
     
+#For loop to compare the dates between the one before and figure out the profits gained or lost
+    for i in range(1, len(profitLosses)):
+        minMax.append(profitLosses[i] - profitLosses[i-1])
+        avgProfitLoss = sum(minMax)/months
+        maxProfit = max(minMax)
+        minProfit = min(minMax)
+        maxDate = str(date[profitLosses.index(maxProfit)])
+        minDate = str(date[profitLosses.index(minProfit)])
 
 
+#Outputting results
 
     print("Financial Analysis")
-    print("----------------------")
+    print("---------------------------")
     print(f"Total Months: {months}")
+    print(f"Total: ${totalProfit}")
+    print(f"Average Change: ${avgProfitLoss}")
+    print(f"Greatest Increase in Profits: {maxDate} (${maxProfit})")
+    print(f"Greatest Decrease in Profits:  ")
+
+
     
 
