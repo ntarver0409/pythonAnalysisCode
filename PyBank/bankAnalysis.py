f0765d2d817a46c56b12fe628de92022328bc3c2
ntarver0409 <nctarver4994@icloud.com>
2020-11-10 16:29:31 -0600

    Updates to bankAnalysis code.

--- PyBank/bankAnalysis.py
+++ PyBank/bankAnalysis.py
@@ -5,13 +5,17 @@ import csv
 #defining pathway
 csvpath = os.path.join('Resources', 'budget_data.csv')
 
-def bankTotals(bankData)
-    months = int(bankData[0])
-    gainsLosses = int(bankData[1])
-
-    
-
-with open(csvpath) as bankRecords
+with open(csvpath, 'r') as bankRecords:
 
     csvreader = csv.reader(bankRecords, delimiter = ',')
+    header = next(csvreader)
+    lines = len(list(csvreader))
+    
+
+
+
+    print("Financial Analysis")
+    print("----------------------")
+    print(f"Total Months: {lines}")
+    print
 
