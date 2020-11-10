9a0750eef17ffb7f240606c70d79d5fefac8939a
ntarver0409 <nctarver4994@icloud.com>
2020-11-10 17:14:40 -0600

    Saving progress on bankAnalysis.py

--- PyBank/bankAnalysis.py
+++ PyBank/bankAnalysis.py
@@ -8,14 +8,33 @@ csvpath = os.path.join('Resources', 'budget_data.csv')
 with open(csvpath, 'r') as bankRecords:
 
     csvreader = csv.reader(bankRecords, delimiter = ',')
+ #skipping header row   
     header = next(csvreader)
-    lines = len(list(csvreader))
+#defiining variables 
+    months = len(list(csvreader)) #reads all the rows in the csv and outputs that number
+    dates = []
+    profitLosses = []
+    minMax = []
+
+#For loop to cycle through each column of data and assign the column to the variable array
+    for row in bankRecords:
+        dates.append(row[0])
+        profitLosses.append(row[1])
+
+    for i in range(1, len(minMax)):
+
+
+
+
+
+
+
     
 
 
 
     print("Financial Analysis")
     print("----------------------")
-    print(f"Total Months: {lines}")
-    print
+    print(f"Total Months: {months}")
+    
 
