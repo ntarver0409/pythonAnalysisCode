6a7b01999cfe44ef92a8c653157215b1d8c43d91
ntarver0409 <nctarver4994@icloud.com>
2020-11-12 17:00:47 -0600

    Latest version of bankAnalysis.py

--- PyBank/bankAnalysis.py
+++ PyBank/bankAnalysis.py
@@ -5,43 +5,45 @@ import csv
 #defining pathway
 csvpath = os.path.join('Resources', 'budget_data.csv')
 
-with open(csvpath, 'r') as bankRecords:
+with open(csvpath) as bankRecords:
 
-    csvreader = csv.reader(bankRecords, delimiter = ',')
+    csvreader = csv.reader(bankRecords)
  #skipping header row   
     header = next(csvreader)
-#defiining variables 
+#defiining variables
     months = len(list(csvreader)) #reads all the rows in the csv and outputs that number
     dates = []
-    profitLosses = []
-    minMax = []
-    totalProfit = sum(profitLosses)
+    profitLoss = []
+    change = []
+    
 
 #For loop to cycle through each column of data and assign the column to the variable array
     for row in bankRecords:
         dates.append(row[0])
-        profitLosses.append(float(row[1])
+        profitLoss.append(float(row[1]))
+    
+    totalProfit = sum(profitLoss)
 
     
 #For loop to compare the dates between the one before and figure out the profits gained or lost
-    for i in range(1, len(profitLosses)):
-        minMax.append(profitLosses[i] - profitLosses[i-1])
-        avgProfitLoss = sum(minMax)/months
-        maxProfit = max(minMax)
-        minProfit = min(minMax)
-        maxDate = str(date[profitLosses.index(maxProfit)])
-        minDate = str(date[profitLosses.index(minProfit)])
+    for i in range(1, len(profitLoss)):
+        change.append(profitLoss[i+1] - profitLoss[i])
+        avgProfitLoss = sum(change)/len(change)
+        maxProfit = max(change)
+        minProfit = min(change)
+        maxDate = str(dates[profitLoss.index(maxProfit)])
+        minDate = str(dates[profitLoss.index(minProfit)])
 
 
 #Outputting results
 
-    print("Financial Analysis")
+    print(f"Financial Analysis")
     print("---------------------------")
     print(f"Total Months: {months}")
     print(f"Total: ${totalProfit}")
-    print(f"Average Change: ${avgProfitLoss}")
+    print(f"Avereage Change: ${avgProfitLoss}")
     print(f"Greatest Increase in Profits: {maxDate} (${maxProfit})")
-    print(f"Greatest Decrease in Profits:  ")
+    print(f"Greatest Decrease in Profits: {minDate} (${minProfit})")
 
 
     
