bf10b200ca594808c381c1b630cda3800e750c7a
ntarver0409 <nctarver4994@icloud.com>
2020-11-13 18:11:36 -0600

    Progress on pollAnalysis.py

--- PyPoll/pollAnalysis.py
+++ PyPoll/pollAnalysis.py
@@ -13,12 +13,47 @@ with open(csvpath) as electionData:
     header = next(csvreader)  
     votes = []
     canidates = []
+    khanCount = 0
+    correyCount = 0
+    liCount = 0
+    tooleyCount = 0
 
-    for row in electionData:
+    for row in csvreader:
         votes.append(row[0])
         canidates.append(row[2])
+        #Counting the number of votes of each canidate
+        if row[2] == "Khan":
+            khanCount = khanCount + 1
+        elif row[2] == "Correy":
+            correyCount = correyCount + 1
+        elif row[2] == "Li":
+            liCount = liCount + 1
+        else:
+            tooleyCount = tooleyCount + 1
+            
     
     numVotes = len(votes)
+#Finding the canidates vote percentage and formatting it 
+    khanPercent = "{:.3%}".format(khanCount/numVotes)
+    correyPercent = "{:.3%}".format(correyCount/numVotes)
+    liPercent = "{:.3%}".format(liCount/numVotes)
+    tooleyPercent = "{:.3%}".format(tooleyCount/numVotes)
+
+
+
+
+
+
+
+
+
+print(f"Election Results")
+print("-------------------------")
+print(f"Total Votes: {numVotes}")
+print("-------------------------")
+print(f"Khan: {khanPercent} ({khanCount})")
+
+
      
 
 
